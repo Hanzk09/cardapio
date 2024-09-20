fa7b2925fa8b2626f7c2290308f3f205c35f868d
hanz.kassik <hanzkassik07@hotmail.com>
2024-09-20 20:55:41 -0300

    fix: Ajustado validade de token para 30 dias

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     JwtModule.register({
       secret: process.env.JWTCONSTANTKEY,
       global: true,
-      signOptions: { expiresIn: '30m' },
+      signOptions: { expiresIn: '30d' },
     }),
   ],
   controllers: [AuthController],
